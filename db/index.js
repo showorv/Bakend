5f4b654a241c370c2e60c5705d49fdacfbfee497
Showrov <yshowrov7@gmail.com>
2024-04-27 23:48:59 +0600

    add db/index.js

---
+++ db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+// import bodyparserJsonError from "bodyparser-json-error";
+
+const connectDB = async()=>{
+    try{
+        const connectionins= await mongoose.connect(`${process.env.Mongodb_URI}/${DB_NAME}`);
+        console.log(`mongodb conneted:: Db host:${connectionins.connection.host}`);
+    }catch(error){
+        console.log("Error",error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
+
+
+
+
+
+//process hcche current application er reference
