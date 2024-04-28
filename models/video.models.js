967882fa102d5ef3599a6eca2a5a662250c19563
Showrov <yshowrov7@gmail.com>
2024-04-28 19:38:05 +0600

    add models config

---
+++ models/video.models.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new mongoose.Schema({
+    videofile:{
+        type: String, //url
+        required: true
+    },
+    thumbail:{
+        type: String, //url
+        required: true
+    },
+    tittle:{
+        type: String, 
+        required: true
+    },
+    tdescription:{
+        type: String, 
+        required: true
+    },
+    tittle:{
+        type: Number, 
+        required: true
+    },
+
+    views:{
+        type: Number,
+        default:0
+    },
+
+    isPublished:{
+        type:Boolean,
+    default:true
+    },
+    owner:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video= mongoose.model("Video",videoSchema)
